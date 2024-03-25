f20eb37f8173ec04cc6f643740131236dc55a654
Helom Valentim <helomvalentim@id.uff.br>
2024-03-25 15:42:43 +0000

    Update importExcelReferences.js

--- src/importExcelReferences.js
+++ src/importExcelReferences.js
@@ -1,30 +1,3 @@
-/*
-    Ficheiro utilizado como referencia para importar dados de ficheiros excel utilizando a função "\api\utils\importExcelToSql.js" a estrutura a ser utilizada é separada pelas
-    sheets que se desejam importar. Cada sheet tem um array de objetos que contem as informações necessárias para importar os dados para a base de dados.
-    A estrutura é a seguinte:
-        {
-            name: "nome da tabela na base de dados",
-            worksheetIndex: 0, //index da sheet no ficheiro excel que será utilizado caso o nome da sheet não seja encontrado.
-            worksheetName: "nome da sheet",
-            deleteTable: true, //se a tabela deve ser apagada antes de inserir os dados. Se false, os dados são adicionados à tabela existente.
-            filePath: 'caminho do ficheiro', //caminho do ficheiro excel em geral ficam na pasta "//WSSPADAF001/Drops/Uploads/..pasta do projeto/..nome do ficheiro.xlsx"
-            headerRow: 0, //linha onde se encontram os nomes das colunas (começa do zero)
-            headers:
-                [
-                    {
-                        name: "nome da coluna na base de dados",
-                        columnType: "tipo de dados da coluna (SQL Type)",
-                        excelColumnName: "nome da coluna no excel",
-                        excelColumnIndex: 0, //index da coluna no excel começa do zero. É utilizado caso o nome da coluna não seja encontrado.
-                    },
-                    ... //Manter o mesmo padrão para todas as colunas. 
-                ]
-        }
-
-    Para importar mais de uma sheet do mesmo ficheiro, basta criar um novo objeto com as informações necessárias mantendo o mesmo filePath.
-    Somente serão importadas as colunas que estiverem presentes no array de headers. Caso a coluna não seja encontrada, será ignorada.
-*/
-
 const customers_list = [
     {
         name: "customers_list",
